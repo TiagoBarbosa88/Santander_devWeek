584c7ced3e091cb55793cc8637e83db12f740b80
TiagoBarbosa88 <sombr43@gmail.com>
2023-07-30 21:55:13 -0300

    update project

--- santander-dev-week-angular/src/app/services/cards.service.ts
+++ santander-dev-week-angular/src/app/services/cards.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environments } from '../environments/environments';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardsService {
-	
-  constructor() { }
+	private apiURL = environments.API_URL
+
+  constructor(private http: HttpClient) { }
+
+	getCard(): Observable<any>{
+		return this.http.get<any>(this.apiURL)
+	}
 }
